bda62e0f0cc38ad3b737f844c849af0b0f3bd415
Tintinabulum <60529494+Tintinabulum@users.noreply.github.com>
2022-02-09 16:32:23 -0500

    More Functionality
    Added some more TokenType
    Added some cases in scanToken()

--- Project1/src/main/java/compiler/project1/CMinusScanner.java
+++ Project1/src/main/java/compiler/project1/CMinusScanner.java
@@ -1,5 +1,6 @@
 package compiler.project1;
 import java.io.BufferedReader;
+import java.io.IOException;
 public class CMinusScanner implements Scanner {
     private BufferedReader inFile;
     private Token nextToken;
@@ -7,9 +8,7 @@ public class CMinusScanner implements Scanner {
         inFile = file;
         nextToken = scanToken();
     }
-    public Token scanToken(){
-        return null; //NEEDS TO BE IMPLEMENTED
-    }
+    
     public Token getNextToken () {
         Token returnToken = nextToken;
         if (nextToken.getType() != Token.TokenType.EOF)
@@ -18,5 +17,37 @@ public class CMinusScanner implements Scanner {
     }
     public Token viewNextToken () {
         return nextToken;
+    }
+    
+    public Token scanToken(){
+        try{
+            int next = inFile.read();
+            while(next!=-1){//-1 means EOF
+                switch((char)next){
+                    case '+': return new Token(Token.TokenType.ADD);
+                    case '-': return new Token(Token.TokenType.SUB);
+                    case '*': return new Token(Token.TokenType.MULT);
+                    case '/':
+                        inFile.mark(0);
+                        next = inFile.read();
+                        if((char)next!='*'){
+                            inFile.reset();
+                            return new Token(Token.TokenType.DIV);
+                        }
+                        boolean wasStar = false;
+                        next = inFile.read();
+                        while(!(wasStar && (char)next=='/')){
+                            wasStar=next=='*';
+                            next = inFile.read();
+                        }
+                        return new Token(Token.TokenType.COMMENT);
+                    //Needs more cases
+                    default: return new Token(Token.TokenType.ERROR,"Unknown symbol: "+(char)next);
+                }
+            }
+        }catch(IOException error){//Can happen for comments
+             return new Token(Token.TokenType.BEGCOM);
+        }
+        return new Token(Token.TokenType.EOF); 
     }
 }
--- Project1/src/main/java/compiler/project1/Token.java
+++ Project1/src/main/java/compiler/project1/Token.java
@@ -3,11 +3,11 @@ package compiler.project1;
 public class Token {
     public enum TokenType{
         ELSE, IF, INT, RETURN, VOID, WHILE, //Page 491 #1
-        ADD, SUB, MULT, DIV, LESS, LESSEQUAL, GREATER, GREATEREQUAL, //Page 491 #2
+        ADD, SUB, MULT, DIV, LESS, LESSEQU, GRE, GREEQU, //Page 491 #2
         EQUAL, NOTEQUAL, ASSIGN, SEMICOLON, COMA, BEGPAR, ENDPAR, //Page 491, #2
         BEGSBRA, ENDSBRA, BEGBRA, ENDBRA, BEGCOM, ENDCOM, //Page 491, #2
         ID, NUM, //Page 492 #3
-        EOF
+        EOF, ERROR, COMMENT
     }
     private TokenType tokenType;
     private Object tokenData;
